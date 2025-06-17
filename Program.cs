aff1e32cfeb8f17e8aa2d9f36e20635160f40bc7
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-17 17:44:44 +0200

    Remove weather forecast functionality and refactor setup
    This commit removes the entire weather forecast functionality, including the `WeatherForecastController` and `WeatherForecast` class. The `Program.cs` file has been refactored to streamline the `Main` method, moving database connection setup and service registrations outside of the development environment check for improved efficiency. Swagger setup and data seeding logic are now executed unconditionally in development.

--- Program.cs
+++ Program.cs
@@ -3,43 +3,42 @@ using DjurAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
-namespace DjurAPI
+namespace DjurAPI;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        // Database connection
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<DjurContext>(
+            options => options.UseSqlServer(connectionString));
+
+        // Add services to the container.
+        builder.Services.AddTransient<DAL.TransactionManager>();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-            // Database connection
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<DjurContext>(
-                options => options.UseSqlServer(connectionString));
-
-            // Add services to the container.
-            builder.Services.AddTransient<DAL.TransactionManager>();
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-                await app.SeedDataAsync();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            await app.SeedDataAsync();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
